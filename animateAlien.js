75aa17120fa01f3a6931b6d441771c93aa6e0a9f
faithchan <fch@mercantilemaritime.com>
2021-09-16 15:34:22 +0800

    added instructions modal popup

--- animateAlien.js
+++ animateAlien.js
@@ -1,14 +1,27 @@
 const aliens = document.querySelectorAll('.alien');
 const holes = document.querySelectorAll('.hole');
+const startButton = document.querySelector('#startBtn')
 let lastHole;
 //Pick Random hole and make sure it does not repeat
 function pickRandomHole(holes) { 
     const randomHole = Math.floor(Math.random()*holes.length );
     const hole = holes[randomHole];
     if(hole === lastHole){
-        return pickRandomHole(hole)
+        return pickRandomHole(holes)
     }
     lastHole = hole;
     return hole;
-
 }
+
+//Add extra class to the currently selected hole
+function popOut(){
+    const time= Math.random()* 1300 + 400;
+    const hole = pickRandomHole(holes);
+    hole.classlist.add('up');
+    setTimeout(function(){
+        hole.classlist.remove('up')
+        if(!timeUp) popOut();
+    }, time)
+}
+
+popOut();
