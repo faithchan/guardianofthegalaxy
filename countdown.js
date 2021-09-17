aa20d59afc66aacd61711e9f78a7ec3af262b8ce
faithchan <fch@mercantilemaritime.com>
2021-09-17 23:03:17 +0800

    countdown timer limit set to 0

--- countdown.js
+++ countdown.js
@@ -1,12 +1,14 @@
 let countTime = 30;
-const countdown = document.querySelector('.countdown')
+const countdown = document.querySelector('.countdown');
 
+setInterval(updateCountdown, 1000);
 function updateCountdown() {
     countdown.innerHTML = countTime;
     countTime --;
+    countTime = countTime < 0 ? 0 : countTime; 
 };
 
-setInterval(updateCountdown, 1000);
+
 //if time = 0 and points <= highscore, hide .game display time is up you've scored xx points. append replay button
 //if points> highscore, times up! You've achieved highscore, thank you for saving our galaxy. append replay button
 
