7637a35528cf9e08df3c27a168f27c7d95623a25
faithchan <fch@mercantilemaritime.com>
2021-09-21 15:26:46 +0800

    animateAliens js now functionable, aliens pop out when function is invoked

--- animateAlien.js
+++ animateAlien.js
@@ -3,19 +3,23 @@ const holes = document.querySelectorAll('.hole');
 const startButton = document.querySelector('#startBtn')
 let lastHole;
 
+//score
+const yourScore = document.querySelector('.yourscore')
+let gameScore = 0;
+
 
 //Pick Random hole and make sure it does not repeat
 
 //get random time 
 function randomTime(min, max){
-    return Math.floor(Math.random()*(max-min)*min)
+    return Math.floor(Math.random()*(max-min)+min)
 }
 
 //pick a random hole for the alien to pop up and make sure it does not repeat
 //holes returns a node list of 6 holes, need to get a random index of this array
 function randomHole(holes){
-    const index = Math.floor(Math.random()*holes.length);
-    const hole = holes[index];
+    const getRandomIndex = Math.floor(Math.random()*holes.length);
+    const hole = holes[getRandomIndex];
     // the above gets a repeat hole since its 1 in 6 chances. 
     if (hole == lastHole){
         randomHole(holes)
@@ -26,3 +30,8 @@ function randomHole(holes){
 }
 
 //get aliens to pop out randomly
+function alienPopOut(){
+    const time = randomTime (200, 1000);
+    const hole = randomHole(holes);
+    hole.classList.add('up');
+}
--- score.js
+++
@@ -1,8 +0,0 @@
-const yourScore = document.querySelector('.yourscore')
-const alien = document.querySelectorAll('.hole')
-
-let gameScore = 0;
-
-function alienKilled(){
-
-}
